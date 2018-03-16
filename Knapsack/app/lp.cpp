bc15a9ad9d8cf607be2c033cd4e4126b2dac2825
Anton Logunov <antoshkaplus@gmail.com>
2018-03-16 07:56:23 -0400

    ks lp. can't figure out how to guide my search.

--- Knapsack/app/lp.cpp
+++ Knapsack/app/lp.cpp
@@ -5,24 +5,27 @@
 using namespace ant;
 
 int main(int argc, const char *argv[]) {
-    auto path = "data/ks_100_0";
+    auto path = "data/ks_19_0";
     std::ifstream in(path);
     auto pr = ReadProblem(in);
 
-    CoinModel coinModel;
-    for (auto item : pr.items) {
-        const int row = 0;
-        double w = item.w;
-        coinModel.addCol(1, &row, &w, 0, 1, item.v, NULL, true);
-    }
-    coinModel.setRowUpper(0, pr.capacity);
+    KS_LP solver;
+    auto sol = solver.solve(pr.items, pr.capacity);
 
-    OsiClpSolverInterface solver;
-    solver.loadFromCoinModel(coinModel);
-    solver.setObjSense(-1);
-
-    solver.initialSolve();
-
-    ant::Println(cout, solver.getObjValue());
-    ant::Println(cout, relaxation(pr.items, pr.capacity));
+//    CoinModel coinModel;
+//    for (auto item : pr.items) {
+//        const int row = 0;
+//        double w = item.w;
+//        coinModel.addCol(1, &row, &w, 0, 1, item.v, NULL, true);
+//    }
+//    coinModel.setRowUpper(0, pr.capacity);
+//
+//    OsiClpSolverInterface solver;
+//    solver.loadFromCoinModel(coinModel);
+//    solver.setObjSense(-1);
+//
+//    solver.initialSolve();
+//
+//    ant::Println(cout, solver.getObjValue());
+//    ant::Println(cout, relaxation(pr.items, pr.capacity));
 }
--- Knapsack/include/lp.hpp
+++ Knapsack/include/lp.hpp
@@ -4,6 +4,9 @@
 #include <coin/CoinModel.hpp>
 #include <coin/OsiClpSolverInterface.hpp>
 #include <coin/CbcModel.hpp>
+#include <coin/CbcSimpleInteger.hpp>
+#include <coin/CglKnapsackCover.hpp>
+
 
 using namespace std;
 
@@ -25,16 +28,65 @@ public:
         }
         coinModel.setRowUpper(0, capacity);
 
+        cout << "cap:" << capacity << "\n";
+
         OsiClpSolverInterface solver;
         solver.loadFromCoinModel(coinModel);
         solver.setObjSense(-1);
 
         CbcModel model(solver);
-//        model.setLogLevel(0);
+        //model.setLogLevel(0);
+        CglKnapsackCover generator;
+        //model.addCutGenerator(&generator, 0, "knapsack");
+
+
+        model.initialSolve();
+
+        setPriority(model, items);
+
+
+        const double *initSolution = model.solver()->getColSolution();
+        vector<double> initRes(initSolution, initSolution+items.size());
+
+        ant::Println(cout, initRes, std::string("relax"));
+
         model.branchAndBound();
         const double *solution = model.bestSolution();
 
         vector<int> res(solution, solution+items.size());
         return {abs(model.getSolverObjValue()), res};
     }
+
+private:
+    void setPriority(CbcModel& model, const vector<Item>& items) {
+        std::vector<CbcSimpleInteger*> objs;
+
+        ant::Println(cout, effectiveness(items), std::string("effect"));
+        auto k = 0;
+
+        for (auto i : items) {
+            cout << k++ <<","<< i.w << " ";
+        }
+        cout << "\n";
+
+        int priority = items.size();
+        for (auto c : effectiveness(items)) {
+
+            auto obj = new CbcSimpleInteger(&model, c);
+            obj->setPriority(100*priority);
+            obj->setPreferredWay(1);
+            obj->setColumnNumber(c);
+
+            --priority;
+
+            // go up first
+            objs.push_back(obj);
+        }
+        model.addObjects(objs.size(), (CbcObject**)objs.data());
+
+        for (auto obj : objs) {
+            delete obj;
+        }
+    }
+
 };
